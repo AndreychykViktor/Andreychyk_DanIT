dc5439ba6907334e9c3cf162bca25d8fe1b88203
AndreychykViktor <andreychik.viktor06@gmail.com>
2025-04-30 20:28:32 +0200

    hm1

---
+++ src/hm2/hm2.java
@@ -0,0 +1,70 @@
+package hm2;
+
+import java.util.Scanner;
+
+public class hm2 {
+    static void Game2(String name) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Let the game begin!");
+        int[] history_of_numbers = new int[101];
+        int user_number;
+        int interarations = 1;
+        int randomNumber = (int) (Math.random() * 100);
+        while(true) {
+            String numberString = sc.nextLine();
+            try {
+                user_number = Integer.parseInt(numberString);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number format");
+                continue;
+            }
+
+            history_of_numbers[user_number]++;
+            if (user_number == randomNumber) {
+                System.out.println("Congratulations " + name);
+                System.out.println("Random number was: " + randomNumber);
+                break;
+            }
+            if (user_number > 100 || user_number < 0) {
+                System.out.println("Please enter a number between 0 and 100");
+            }
+            if (user_number > randomNumber) {
+                System.out.println("Your number is too high. Please, try again..");
+
+            }
+            if (user_number < randomNumber) {
+                System.out.println("Your number is too low. Please, try again..");
+
+            }
+            interarations++;
+        }
+        System.out.print("Your numbers: ");
+        for (int i = history_of_numbers.length - 1; i > 0; i--) { //need to use this method for controling {0}
+            if(history_of_numbers[i] > 0) {
+                System.out.printf("%d ", i);
+            }
+        }
+        System.out.println();
+        System.out.println("Count of try " + interarations);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+
+        Game2(name);
+
+    }
+}
